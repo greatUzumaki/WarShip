7d0deb490b5a90a1539f73c16e3737d6d626309a
Alexey Pargachev <pargachev20@gmail.com>
2020-11-24 15:29:00 +0700

    Add files via upload

---
+++ Palubs/MnogoPalub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarShip
+{
+    interface IMnogoPalub
+    {
+        List<Paluba> SpisokPalub();
+
+    }
+}
---
+++ Palubs/Paluba.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarShip
+{
+    class Paluba
+    {
+        int x, y;
+        IMnogoPalub mnogo;
+        public bool DorL;
+
+        public Paluba(int x,int y,IMnogoPalub boss)
+        {
+            this.x = x;
+            this.y = y;
+            mnogo = boss;
+            DorL = true;
+        }
+
+        public void Death(int x,int y)
+        {
+           if(this.x == x && this.y == y)
+            {
+                DorL = false;
+            }
+        }
+
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
+
+
+    }
+}
